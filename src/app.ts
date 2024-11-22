9d1109d6fb6ae2ff6b178d9c60d86621bd1ae765
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-22 17:29:09 +0600

    define product route

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import productRoute from './app/module/product/product.route';
 
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+
+//application routes
+app.use('/api/products', productRoute);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Home Page');

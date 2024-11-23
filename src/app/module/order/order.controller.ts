82946f179eaf187c5f1f46fd9812f96d0b7af301
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-23 20:59:41 +0600

    create order total revenue controller

--- src/app/module/order/order.controller.ts
+++ src/app/module/order/order.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from 'express';
 
 import { OrderService } from './order.service';
 
+// Create a new order
 const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const orderData = req.body;
@@ -18,6 +19,27 @@ const createOrder = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
+// calculate order revenue
+const calculateRevenue = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    // get total revenue
+    const result = await OrderService.calculateTotalRevenue();
+
+    res.status(200).json({
+      message: 'Order created successfully',
+      success: true,
+      data: result,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const OrderController = {
   createOrder,
+  calculateRevenue,
 };

2bbc9e1ebbef539e9c1f555b259d777c0fde181f
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-23 17:25:23 +0600

    create controller for update specific product

--- src/app/module/product/product.controller.ts
+++ src/app/module/product/product.controller.ts
@@ -67,6 +67,42 @@ const getProductById = async (
   }
 };
 
+// Update a specific product by id
+const updateProduct = async (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    const { productId } = req.params;
+    const updateData = req.body;
+
+    // Update product in the database
+    const updatedProduct = await ProductService.updatedProductInDB(
+      productId,
+      updateData,
+    );
+
+    // if product not found in the database
+    if (!updatedProduct) {
+      res.status(404).json({
+        message: 'Product not found',
+        success: false,
+        data: null,
+      });
+    }
+
+    // if product found in the database
+    res.status(200).json({
+      message: 'Product updated successfully',
+      success: true,
+      data: updatedProduct,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
 export const ProductController = {
   createProduct,
   getAllProducts,

a648fc8ca4d5e5a652b118e3fc6c9f0d3e22a748
Juboraj Sujon <mdsujon72@gmail.com>
2024-11-23 21:01:24 +0600

    assign route for total revenue

--- src/app/module/order/order.route.ts
+++ src/app/module/order/order.route.ts
@@ -4,6 +4,6 @@ import { OrderController } from './order.controller';
 const router = express.Router();
 
 router.get('/', OrderController.createOrder);
-// router.get('/revenue');
+router.get('/revenue', OrderController.calculateRevenue);
 
 export const OrderRoutes = router;

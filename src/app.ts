7a528e683bb20578dcc4211293901dde6d466529
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-17 09:29:46 +0600

    Added cookie parser package

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,16 @@
 // const express = require('express')
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
-import { errorHandler } from './app/module/errorHandler/errorHandler';
+import cookieParser from 'cookie-parser';
 import router from './app/routers';
+import { globalErrorHandler } from './app/middlwares/globalErrorHandler';
 
 const app: Application = express();
 
 //parser
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 
 //application routes
 app.use('/api', router);
@@ -17,6 +19,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Home Page');
 });
 
-app.use(errorHandler);
+app.use(globalErrorHandler);
 
 export default app;

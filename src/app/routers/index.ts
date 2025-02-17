1c867ea3b73fa98ffaa56d5c2a8ff06781ca23b4
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-17 20:36:18 +0600

    Added AuthRouter

--- src/app/routers/index.ts
+++ src/app/routers/index.ts
@@ -2,14 +2,15 @@ import { Router } from 'express';
 import { UserRoutes } from '../module/user/user.route';
 import { OrderRoutes } from '../module/order/order.route';
 import { ProductRoutes } from '../module/product/product.route';
+import { AuthRoutes } from '../module/auth/auth.route';
 
 const router = Router();
 
 const moduleRoutes = [
-  // {
-  //   path: '/auth'
-  //   route: AuthRoutes,
-  // },
+  {
+    path: '/auth',
+    route: AuthRoutes,
+  },
 
   {
     path: '/users',

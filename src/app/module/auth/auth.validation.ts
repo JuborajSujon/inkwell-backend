7a0857eea93d11197cf38ae4642627f126efd296
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-18 11:34:38 +0600

    Fix login validation schema email feild

--- src/app/module/auth/auth.validation.ts
+++ src/app/module/auth/auth.validation.ts
@@ -18,7 +18,7 @@ const registerUserValidationSchema = z.object({
 
 const loginValidatioinSchema = z.object({
   body: z.object({
-    email: z.string({ required_error: 'Email is required' }),
+    email: z.string({ required_error: 'Email is required' }).email(),
     password: z.string({ required_error: 'Password is required' }),
   }),
 });

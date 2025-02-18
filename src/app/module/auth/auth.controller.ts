8d7b3dcfbf3f736b5ccf08206be84b317c3826e2
Juboraj Sujon <mdsujon72@gmail.com>
2025-02-18 11:35:27 +0600

    Added login user functionality

--- src/app/module/auth/auth.controller.ts
+++ src/app/module/auth/auth.controller.ts
@@ -2,17 +2,27 @@ import status from 'http-status';
 import catchAsync from '../../utils/CatchAsync';
 import sendResponse from '../../utils/sendResponse';
 import { AuthService } from './auth.service';
+import config from '../../config';
 
 const loginUser = catchAsync(async (req, res) => {
   // Login user
   const result = await AuthService.loginUser(req.body);
+
+  const { accessToken, refreshToken } = result;
+
+  res.cookie('refreshToken', refreshToken, {
+    secure: config.NODE_ENV === 'development',
+    httpOnly: true,
+    sameSite: 'none',
+    maxAge: 1000 * 60 * 60 * 365,
+  });
 
   // Send Response
   sendResponse(res, {
     statusCode: status.OK,
     success: true,
     message: 'User is logged in successfully!',
-    data: {},
+    data: { accessToken },
   });
 });
 
--- src/app/module/auth/auth.service.ts
+++ src/app/module/auth/auth.service.ts
@@ -3,7 +3,10 @@ import AppError from '../../errors/AppError';
 import { User } from '../user/user.model';
 import { TLoginUser } from './auth.interface';
 import { TUser } from '../user/user.interface';
+import { createToken } from './auth.utils';
+import config from '../../config';
 
+// create a new user
 const registerUser = async (payload: TUser) => {
   //check if the user exists
   const user = await User.isUserExistByEmail(payload?.email);
@@ -16,18 +19,52 @@ const registerUser = async (payload: TUser) => {
   return result;
 };
 
+// login user
 const loginUser = async (payload: TLoginUser) => {
   //check if the user exists
   const user = await User.isUserExistByEmail(payload?.email);
 
   if (!user) throw new AppError(status.NOT_FOUND, 'This user is not found !');
 
+  // check is the user is active or not
+  const isActive = user?.status;
+  if (isActive === 'inactive')
+    throw new AppError(status.UNAUTHORIZED, 'This user is not active !');
+
   // checking if the user is already blocked
   const isBlocked = user?.isBlocked;
+  if (isBlocked) throw new AppError(status.FORBIDDEN, 'This user is blocked !');
 
-  if (!isBlocked)
-    throw new AppError(status.FORBIDDEN, 'This user is blocked !');
+  // checking if the password is correct
+  if (!(await User.isPasswordMatched(payload?.password, user?.password))) {
+    throw new AppError(status.FORBIDDEN, 'Password does not match !');
+  }
+
+  const jwtPayload = {
+    email: user?.email,
+    role: user?.role,
+  };
+
+  // create token and send to the client
+  const accessToken = createToken(
+    jwtPayload,
+    config.jwt_access_secret as string,
+    config.jwt_access_expires_in as string,
+  );
+
+  // refresh token
+  const refreshToken = createToken(
+    jwtPayload,
+    config.jwt_refresh_secret as string,
+    config.jwt_refresh_expires_in as string,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
+
 export const AuthService = {
   registerUser,
   loginUser,
